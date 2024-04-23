ec0bb84f37b96b17e6c45533413bfba92caef0a7
Luis Ardon <netoardon9@gmail.com>
2024-04-23 01:19:39 -0600

    feat: interceptor para capturar datos sobre el solicitante de cada endpoint (INTERCEPTOR-GLOBAL)

---
+++ src/core/application/interceptors/loggin.interceptor.ts
@@ -0,0 +1,41 @@
+import { LoggerService } from '@/infrastructure/providers/logger.service';
+import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
+import { Observable, tap } from 'rxjs';
+
+export class LogginInterceptor implements NestInterceptor {
+  constructor(private readonly logger: LoggerService) {}
+
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const now = Date.now();
+    const httpContext = context.switchToHttp();
+    const request = httpContext.getRequest();
+
+    const ip = this.getIP(request);
+
+    this.logger.log(
+      `Incoming Request on ${request.path}`,
+      `method = ${request.method} ip=${ip}`,
+    );
+
+    return next.handle().pipe(
+      tap(() => {
+        this.logger.log(
+          `End Request for ${request.path}`,
+          `method = ${request.method} ip=${ip} duration = ${Date.now() - now}ms`,
+        );
+      }),
+    );
+  }
+
+  private getIP(request: any): string {
+    let ip: string;
+    const ipAddr = request.headers['x-forwarded-for'];
+    if (ipAddr) {
+      const list = ipAddr.split(',');
+      ip = list[list.length - 1];
+    } else {
+      ip = request.connection.remoteAddress;
+    }
+    return ip.replace('::ffff:', '');
+  }
+}
