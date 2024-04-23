8e6558e37c5da6c754408797c1d19305f677e82b
Luis Ardon <netoardon9@gmail.com>
2024-04-23 01:26:10 -0600

    fix: se ajusto IV estatico

--- src/infrastructure/providers/criptography.provider.ts
+++ src/infrastructure/providers/criptography.provider.ts
@@ -1,34 +1,41 @@
 import { Injectable } from '@nestjs/common';
 import * as crypto from 'crypto';
-
+/***
+ * EncryptionService class
+ * @class
+ * @constructor algorithm, key, iv
+ * @public
+ * @description Clase que se encarga de manejar la encriptación de las contraseñas
+ * @version 1.0.0
+ * @example
+ * EncryptionService encryptionService = new EncryptionService();
+ * encryptionService.encryptPassword('password');
+ * @returns string
+ * @author Luis Ardón
+ */
 @Injectable()
 export class EncryptionService {
-  private readonly algorithm = 'aes-256-cbc';
+  private readonly algorithm = 'aes-256-cbc'; // AES (Advanced Encryption Standard) - 256 bits - CBC (Cipher Block Chaining);
   private readonly key: Buffer;
+  private readonly iv: Buffer;
 
   constructor() {
     const masterKey = process.env.MASTER_KEY;
     this.key = crypto.createHash('sha256').update(masterKey).digest();
+    this.iv = Buffer.alloc(16, 0);
   }
 
   encryptPassword(password: string): string {
-    const iv = crypto.randomBytes(16);
-    const cipher = crypto.createCipheriv(this.algorithm, this.key, iv);
+    const cipher = crypto.createCipheriv(this.algorithm, this.key, this.iv);
     let encryptedPassword = cipher.update(password, 'utf8', 'base64');
     encryptedPassword += cipher.final('base64');
-    const encryptedData = `${iv.toString('base64')}.${encryptedPassword}`;
-    return Buffer.from(encryptedData, 'binary').toString('base64');
+    return encryptedPassword;
   }
 
   comparePassword(password: string, encryptedPassword: string): boolean {
-    const encryptedData = Buffer.from(encryptedPassword, 'base64').toString(
-      'binary',
-    );
-    const [ivBase64, encryptedPasswordBase64] = encryptedData.split('.');
-    const iv = Buffer.from(ivBase64, 'base64');
-    const decipher = crypto.createDecipheriv(this.algorithm, this.key, iv);
+    const decipher = crypto.createDecipheriv(this.algorithm, this.key, this.iv);
     let decryptedPassword = decipher.update(
-      encryptedPasswordBase64,
+      encryptedPassword,
       'base64',
       'utf8',
     );
